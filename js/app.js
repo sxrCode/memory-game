1f8370c4c03942072a4af2c3b179683579bd2a2d
unknown <maricle01@163.com>
2017-12-07 22:42:43 +0800

    feature: 添加了刷新功能

--- js/app.js
+++ js/app.js
@@ -31,6 +31,7 @@ function Card() {
     this.game = null;
     this.cardEle = null;
     let flag = -1;
+    this.iconClass = "";
 
     this.getFlag = function() {
         return flag;
@@ -41,28 +42,57 @@ function Card() {
 
     this.flip = function() {
         this.isCover = false;
+        $(this.cardEle).addClass("open").addClass("show");
         console.log('card flag: ' + flag);
     };
 
     this.cover = function() {
         this.isCover = true;
+        $(this.cardEle).removeClass("open").removeClass("show");
     };
 
     this.onMatchFailure = function() {
-        this.cover();
+        $(this.cardEle).addClass('dismatch');
+        setTimeout(function() {
+            $(this.cardEle).removeClass('dismatch');
+            this.cover();
+        }.bind(this), 500);
+
     };
 
     this.onMatchSuccess = function() {
+        $(this.cardEle).addClass("match");
         console.log('flag ' + flag + ' match success!');
     };
 
     this.dealClick = function() {
+        console.log("this.isCover: " + this.isCover);
         if (this.isCover) {
             this.flip();
             this.game.judge(this);
         }
 
     };
+}
+
+function GradeModule() {
+    let stars = $(".stars .fa");
+    let maxGrade = stars.length;
+    this.grade = maxGrade;
+    this.step = 0;
+
+    this.setGrade = function(grade) {
+        this.grade = grade;
+
+    };
+
+    function updateStar(grade) {
+        let ne = maxGrade - grade
+        if (ne > 0) {
+
+        }
+
+    }
 }
 
 function Game() {
@@ -73,14 +103,19 @@ function Game() {
     let step = 0;
 
     this.judge = function(card) {
-        if (this.isFirst) {
-            this.firstCard = card;
-            this.isFirst = false;
-        } else {
-            this.secendCard = card;
-            this.isFirst = true;
-            matchCard.call(this);
-        }
+
+        setTimeout(function() {
+            if (this.isFirst) {
+                this.firstCard = card;
+                this.isFirst = false;
+            } else {
+                this.secendCard = card;
+                this.isFirst = true;
+                matchCard.call(this);
+                grade.call(this);
+            }
+        }.bind(this), 500);
+
     };
 
     function matchCard() {
@@ -90,6 +125,16 @@ function Game() {
             dealMatchFailure.call(this);
         }
         step++;
+    }
+
+    function grade() {
+        if (step < 16) {
+            console.log("3 star");
+        } else if (step < 24) {
+            console.log("2 star")
+        } else {
+            console.log('1 star');
+        }
     }
 
     function dealMatchSuccess() {
@@ -121,12 +166,14 @@ function Game() {
 
 function startNewGame() {
     let game = new Game();
+    let icons = ["fa-diamond", "fa-paper-plane-o", "fa-anchor", "fa-bolt", "fa-cube", "fa-leaf", "fa-bicycle", "fa-bomb"];
 
     let cards = [];
     for (let i = 0; i < 8; i++) {
         for (let j = 0; j < 2; j++) {
             let card = new Card();
             card.setFlag(i);
+            card.iconClass = icons[i];
             cards.push(card);
         }
     }
@@ -137,6 +184,7 @@ function startNewGame() {
         let cardEle = cardEles[i];
         let card = cards[i];
         $(cardEle).bind("click", function() { card.dealClick(); });
+        $(cardEle).children(".fa").addClass(card.iconClass);
         card.cardEle = cardEle;
         card.game = game;
     }
@@ -147,6 +195,8 @@ function refresh() {
     for (let i = 0; i < cardEles.length; i++) {
         let cardEle = cardEles[i];
         $(cardEle).unbind();
+        $(cardEle).removeClass().addClass('card');
+        $(cardEle).children(".fa").removeClass().addClass('fa');
     }
     startNewGame();
 }
